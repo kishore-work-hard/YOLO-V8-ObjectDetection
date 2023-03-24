4571903a1bd3bde35a1592c55da27919b0e3ec3f
kishore-work-hard <93419928+kishore-work-hard@users.noreply.github.com>
2023-03-24 14:59:55 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
 model = YOLO("./pre_model/yolov8s.pt")
-model.train(data="custom.yaml", batch=8, imgsz=640, epochs=100, workera=1)
+model.train(data="custom.yaml", batch=8, imgsz=640, epochs=100, workers=1)
